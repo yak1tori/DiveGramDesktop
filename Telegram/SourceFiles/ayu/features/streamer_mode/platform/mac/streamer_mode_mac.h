b267e41fc0f7b2b959e92dac3212e67760cb233d
user.email <you@example.com>
2026-09-16 18:34:00 +0500

    DiveGram: also drop streamer-mode win/mac implementations
    linux/streamer_mode_linux.cpp remains and is the only one built.

--- Telegram/SourceFiles/ayu/features/streamer_mode/platform/mac/streamer_mode_mac.h
+++
@@ -1,9 +0,0 @@
-// This is the source code of DiveGram for Desktop.
-//
-// We do not and cannot prevent the use of our code,
-// but be respectful and credit the original author.
-//
-// Copyright @Radolyn, 2026
-#pragma once
-
-#include "ayu/features/streamer_mode/platform/platform_streamer_mode.h"
